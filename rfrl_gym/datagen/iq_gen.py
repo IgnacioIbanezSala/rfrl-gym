5fd7c5860a4817a60a5cc132b2aeaaebfc767459
Ignacio I S <ignacio.ibanez@mi.unc.edu.ar>
2024-09-29 17:21:53 -0300

    Changes to ig_gen test

--- rfrl_gym/datagen/iq_gen.py
+++ rfrl_gym/datagen/iq_gen.py
@@ -10,7 +10,7 @@ class IQ_Gen:
         self.entity_list = entity_list
         self.entity_modems = self.__init_entity_modems(entity_list)
 
-        self.samples_per_step = 10000
+        self.samples_per_step = 100
         self.noise_std = 0.01
 
         self.fc = np.linspace(-0.5, 0.5, self.num_channels+1)+1/self.num_channels/2
@@ -18,13 +18,13 @@ class IQ_Gen:
 
     def gen_iq(self, actions):
         self.samples = np.roll(self.samples, self.samples_per_step, axis=0)
-        self.samples[0:self.samples_per_step] =  self.rng.normal(0.0, self.noise_std/np.sqrt(2.0), self.samples_per_step) + 1.0j*(self.rng.normal(0.0, self.noise_std/np.sqrt(2.0), self.samples_per_step))
+        #self.samples[0:self.samples_per_step] =  self.rng.normal(0.0, self.noise_std/np.sqrt(2.0), self.samples_per_step) + 1.0j*(self.rng.normal(0.0, self.noise_std/np.sqrt(2.0), self.samples_per_step))
         for k in range(self.num_channels):
             for kk in range(self.num_entities):
                 if actions[kk] == k:
                     if kk != 0:
                         entity = self.entity_list[kk-1]
-
+                        print(self.fc)
                         cent_freq = self.rng.uniform(entity.modem_params['center_frequency'][0],entity.modem_params['center_frequency'][1])
                         start = entity.modem_params['start']
                         duration = entity.modem_params['duration']
@@ -36,6 +36,7 @@ class IQ_Gen:
                         self.samples[int(start*self.samples_per_step):int((start+duration)*self.samples_per_step)] += data
                     else:
                         self.t = np.linspace(0, self.samples_per_step, self.samples_per_step)
+                        print(self.fc)
                         data = (1.0/10.0)*np.exp(1j*2*np.pi*(1/self.num_channels)/2*((self.t-self.samples_per_step/2)**2)/self.samples_per_step)
                         data = data * np.exp(1j*2*np.pi*self.fc[k]*self.t)
                         data[0:100] = np.linspace(0,1,100)*data[0:100]
--- scripts/IQ_Gen_test.py
+++ scripts/IQ_Gen_test.py
@@ -4,14 +4,16 @@ import argparse
 import matplotlib.pyplot as plt
 import rfrl_gym
 import math
+import numpy as np
 
 import rfrl_gym.entities
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--scenario', default='sb3_test_scenario.json', type=str, help='The scenario file to preview in the RFRL gym environment.')
+parser.add_argument('-s', '--scenario', default='entity_test.json', type=str, help='The scenario file to preview in the RFRL gym environment.')
 parser.add_argument('-m', '--gym_mode', default='abstract', type=str, help='Which type of RFRL gym environment to run.')
 parser.add_argument('-e', '--epochs', default=1000, type=int, help='Number of training epochs.')
+parser.add_argument('-t', '--title', default="IQ_gen_test", type=str, help='Title of image to save.')
 args = parser.parse_args()
 
 if args.gym_mode == 'abstract':
@@ -23,8 +25,11 @@ env.reset()
 f_idx = open('scenarios/' + args.scenario)
 scenario_metadata = json.load(f_idx)
 
+img_title = args.title + ".png"
+
+
 Num_Channels = scenario_metadata['environment']['num_channels']
-Samples = []
+
 #Entity = rfrl_gym.entities(entity_label = scenario_metadata['entities'], num_channels = Num_Channels, )
 
 entity_idx = 0
@@ -38,19 +43,36 @@ for entity in scenario_metadata['entities']:
     obj_str += ')'
     entity_list.append(eval(obj_str))      
     entity_list[-1].set_entity_index(entity_idx)
+
+name = obj_str
+
 num_entities = len(entity_list)
+print(num_entities)
 
-action_list = [num_entities]
 
-iq_gen = rfrl_gym.datagen.iq_gen.IQ_Gen(Num_Channels, num_entities, [], entity_list)
+action_list = [None] * num_entities
+
+iq_gen = rfrl_gym.datagen.iq_gen.IQ_Gen(Num_Channels, (num_entities-1), scenario_metadata['render']['render_history'], entity_list)
+iq_gen.reset()
+
+info = { }
+info["step_number"] = 0
+info["Samples"] = np.zeros(10000)
+
+for entity in entity_list:
+            entity.reset(info)     
 
 entity_idx = 0
 for entity in entity_list:
+            print(entity_idx)
+            entity_action = entity.get_action(info)
+            action_list[entity_idx] = entity_action
             entity_idx += 1
-            entity_action = entity.get_action()
-            action_list[num_entities] = entity_action
             
-Samples = iq_gen.gen_iq(action_list)
-
-plt.plot(Samples) 
+print(action_list)
+info["Samples"] = iq_gen.gen_iq(action_list)
+print(len(info["Samples"]))
+plt.plot(np.real(info["Samples"])) 
+plt.title(name, loc='center', wrap=True)
+plt.savefig(img_title)
 plt.show()
